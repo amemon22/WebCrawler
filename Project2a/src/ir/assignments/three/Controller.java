7354a54bb8ffb8e054a00802aeee5261949e41b1
amemon22 <amemon2@gmail.com>
2015-04-24 10:50:34 -0700

    Test

---
+++ Project2a/src/ir/assignments/three/Controller.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+import ir.assignments.three.Crawler;
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+
+public class Controller {
+	public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 7;
+        int politenessDelay = 300;
+        
+        
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        controller.addSeed("http://www.ics.uci.edu/~welling/");
+        controller.addSeed("http://www.ics.uci.edu/");
+        
+        CrawlConfig crawlConfig = new CrawlConfig();
+        crawlConfig.setUserAgentString("UCI Inf141-CS121 crawler 59212236 43513937");
+        crawlConfig.setPolitenessDelay(politenessDelay);
+        
+        
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        
+        controller.start(Crawler.class, numberOfCrawlers);
+    }
+}
+
